d906f478d8962f1c00ece254f3eb45f66806703d
mhn098 <mhn098@ad.unc.edu>
2024-07-11 16:09:03 -0400

    add bootstrap template

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  // styleUrl: '../../assets/styles.css'
 })
 export class AppComponent {
   title = 'frontend';
