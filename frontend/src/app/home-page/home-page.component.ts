b1e7aa56913eb510ebd2ed87e5510c9226d775b9
mhn098 <mhn098@ad.unc.edu>
2024-07-20 14:14:03 -0400

    finish gallery and final touches

--- frontend/src/app/home-page/home-page.component.ts
+++ frontend/src/app/home-page/home-page.component.ts
@@ -14,18 +14,85 @@ export class HomePageComponent {
     component: HomePageComponent,
     canActivate: []
   };
+
+  images: string[] = [
+    '/gallery/hello kitty.jpg',
+    '/gallery/strawberry orange.jpg',
+    '/gallery/strawberry1.jpg',
+    '/gallery/strawberry2.jpg',
+    '/gallery/orange1.jpg',
+    '/gallery/orange2.jpg',
+    '/gallery/winter.png',
+    '/gallery/pink and blue 1.jpg',
+    '/gallery/pink and blue 2.jpg',
+    '/gallery/pink and blue 3.jpg',
+    '/gallery/baseball.png',
+    '/gallery/halloween.png',
+    '/gallery/bluewhite.png',
+    '/gallery/pinkflower.png',
+    '/gallery/yellowflower.png',
+    '/gallery/orangeflower.png',
+    '/gallery/redflower.png',
+    '/gallery/pinkwhiteombre.png',
+    '/gallery/pearlwhite.png',
+    '/gallery/pinkwhite.png',
+    '/imgs/pink and white.png',
+    '/gallery/purplesesa.png',
+    '/gallery/blackbutterfly.png',
+    '/gallery/pinkblack.png',
+    '/gallery/green.png',
+    '/gallery/bloodred.png',
+    '/gallery/clearred.png',
+    '/gallery/clearblack.png',
+    '/gallery/purple.png',
+    '/gallery/yellow.png',
+    '/gallery/winterdesigns.png',
+    '/imgs/millennium nails.jpg',
+    '/imgs/ok.jpg'
+  ];
+
+  currentIndex: number = 0;
   constructor() {}
+  // ngAfterViewInit(): void {
+  //   const imageModal = document.getElementById('imageModal');
+  //   if (imageModal) {
+  //     imageModal.addEventListener('show.bs.modal', function (event: any) {
+  //       const button = event.relatedTarget;
+  //       const imageSrc = button.getAttribute('data-bs-image');
+  //       const modalImage = imageModal.querySelector('#modalImage') as HTMLImageElement;
+  //       if (modalImage) {
+  //         modalImage.src = imageSrc;
+  //       }
+  //     });
+  //   }
+  // }
+
   ngAfterViewInit(): void {
     const imageModal = document.getElementById('imageModal');
     if (imageModal) {
-      imageModal.addEventListener('show.bs.modal', function (event: any) {
+      imageModal.addEventListener('show.bs.modal', (event: any) => {
         const button = event.relatedTarget;
         const imageSrc = button.getAttribute('data-bs-image');
-        const modalImage = imageModal.querySelector('#modalImage') as HTMLImageElement;
-        if (modalImage) {
-          modalImage.src = imageSrc;
-        }
+        this.currentIndex = this.images.indexOf(imageSrc);
+        this.updateModalImage();
       });
     }
+  }
+
+  updateModalImage(): void {
+    const modalImage = document.getElementById('modalImage') as HTMLImageElement;
+    if (modalImage) {
+      modalImage.src = this.images[this.currentIndex];
+    }
+  }
+
+  nextImage(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.updateModalImage();
+  }
+
+  prevImage(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.updateModalImage();
   }
 }
